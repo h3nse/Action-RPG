85423cd642483c7726e8c47da93d003a437ad670
h3nse <henrikf.bj@gmail.com>
2022-11-29 18:21:24 +0100

    Add enemy A.I.

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,17 +1,69 @@
 extends KinematicBody2D
 
+#Load once
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+#Make variables
+export var acceleration = 300
+export var max_speed = 50
+export var friction = 200
+
+#State machine
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+#Make variables
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
-onready var stats = $Stats
+#Set loaded state to IDLE
+var state = IDLE
 
+onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
+onready var sprite = $AnimatedSprite
 
 func _physics_process(delta):
 	#Always moves knockback towards zero(friction)
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, friction * delta)
 	#Makes the bat move when knockback is not zero
 	knockback = move_and_slide(knockback)
+	
+	#Identify which state we're in and run appropriate code
+	match state:
+		IDLE:
+			#move velocity towards zero and seek player
+			velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+			seek_player()
+			
+		WANDER:
+			pass
+		
+		CHASE:
+			#Link player variable
+			var player = playerDetectionZone.player
+			#If player is within radius
+			if player != null:
+				#the direction is the difference in the 2 positions
+				var direction = (player.global_position - global_position).normalized()
+				#Set velocity towards the player
+				velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+			else:
+				#Switch to IDLE when we lose sight of player
+				state = IDLE
+	
+	#Flip to face the correct way
+	sprite.flip_h = velocity.x < 0
+	#Move and remember new vector
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	#If we can see the player, change state to CHASE
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 	#area = the area that entered
 func _on_Hurtbox_area_entered(area):
--- Hit- & hurtboxes/Hurtbox.gd
+++ Hit- & hurtboxes/Hurtbox.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
+#Changeable bool so different scenes can turn off hit effect
+export(bool) var show_hit = true
+
 #Load the effect once
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
 func _on_Hurtbox_area_entered(area):
-	#Create instance of effect
-	var effect = HitEffect.instance()
-	#Get access to the scene we're in
-	var main = get_tree().current_scene
-	#Add the effect to that scene
-	main.add_child(effect)
-	#Set the position
-	effect.global_position = global_position
+	if show_hit: 
+		#Create instance of effect
+		var effect = HitEffect.instance()
+		#Get access to the scene we're in
+		var main = get_tree().current_scene
+		#Add the effect to that scene
+		main.add_child(effect)
+		#Set the position
+		effect.global_position = global_position
